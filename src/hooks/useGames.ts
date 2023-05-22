4e0d0d1ae2203a7b963d1f62754c61738894f5b3
AmGh <altghaderi@gmail.com>
2023-05-22 17:50:22 -0600

    Show loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,49 +3,50 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-  
-
-const useGames=()=>{
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal;
-        
-
-      apiClient
-        .get<FetchGamesResponse>("/games",{signal})
-        .then((response) => {
-          setGames(response.data.results);
-        })
-        .catch((error) => {
-            if (error instanceof CanceledError) return;
-          setError(error.message);
-        });
-  
-      return () => controller.abort();
-    },[]);
-
-    return {games,error};
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    setLoading(true);
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal })
+      .then((response) => {
+        setGames(response.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
 
 export default useGames;
