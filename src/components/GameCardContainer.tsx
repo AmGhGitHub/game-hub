b2136e6ee8d61561b3525038ef61408ab906cee8
AmGh <altghaderi@gmail.com>
2023-05-22 18:20:07 -0600

    Refactor: remove dupliacted style

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,24 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const GameCardContainer = ({ children }: Props) => {
+  return (
+    <Box width="300px" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
